bfca087c97821a265376ae0523ef132980457ba1
Ketan Ambekar <krdambekar@gmail.com>
2025-05-09 10:19:25 +0530

    import issue i guess

--- app/src/main/java/com/finoux/tradesdk/TradeSdkActivity.kt
+++ app/src/main/java/com/finoux/tradesdk/TradeSdkActivity.kt
@@ -3,21 +3,22 @@ package com.finoux.tradesdk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class TradeSdkActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val isin = intent.getStringExtra("isin") ?: "Unknown"
+
+        val isin = intent.getStringExtra("isin") ?: "Unknown ISIN"
 
         setContent {
-            MaterialTheme {
-                Surface {
-                    Text(text = "Stock Details for ISIN: $isin")
-                }
-            }
+            StockDetailScreen(isin)
         }
     }
+}
+
+@Composable
+fun StockDetailScreen(isin: String) {
+    Text(text = "Details for ISIN: $isin")
 }
