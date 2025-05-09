0b77707c5a0ff022385e4e77b4d37b40b81159ea
Ketan Ambekar <krdambekar@gmail.com>
2025-05-09 14:43:07 +0530

    hun ja yr

--- tradeappsdk/build.gradle.kts
+++ tradeappsdk/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    id("maven-publish")
 }
 
 android {
@@ -40,4 +42,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.ketanambekar"
+                artifactId = "tradesdk"
+                version = "1.0.9"
+            }
+        }
+    }
 }
