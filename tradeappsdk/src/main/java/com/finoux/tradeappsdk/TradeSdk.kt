e83389259f05162522905d903f20382fa8e86996
ketan.ambekar <ketan.ambekar@finoux.com>
2025-05-12 11:53:47 +0530

    1.0.17: logs added

--- tradeappsdk/src/main/java/com/finoux/tradeappsdk/TradeSdk.kt
+++ tradeappsdk/src/main/java/com/finoux/tradeappsdk/TradeSdk.kt
@@ -1,9 +1,9 @@
 package com.finoux.tradesdk
 
+import android.util.Log
 import androidx.compose.runtime.*
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
-import androidx.compose.runtime.mutableStateOf
 
 object TradeSdk {
 
@@ -15,11 +15,13 @@ object TradeSdk {
     private var onCloseCallback: (() -> Unit)? = null
 
     fun openStockDetails(isin: String) {
+        Log.d("TradeSdk", "openStockDetails called with ISIN: $isin") // Log when opening stock details
         _startIsin.value = isin
         _shouldShowHost.value = true
     }
 
     fun close() {
+        Log.d("TradeSdk", "close called, hiding stock details") // Log when closing the SDK
         _shouldShowHost.value = false
         onCloseCallback?.invoke()
     }
@@ -29,6 +31,8 @@ object TradeSdk {
         onCloseCallback = onClose
 
         if (_shouldShowHost.value) {
+            Log.d("TradeSdk", "Host composable displayed. Navigating to stock details.") // Log when Host is shown
+
             val navController = rememberNavController()
             _navController = navController
 
@@ -36,10 +40,16 @@ object TradeSdk {
                 composable("stock_details") {
                     val isin = _startIsin.value
                     if (isin != null) {
-                        StockDetailScreen(isin = isin, onBack = { close() })
+                        Log.d("TradeSdk", "Displaying StockDetailScreen for ISIN: $isin") // Log when stock details screen is being displayed
+                        StockDetailScreen(isin = isin, onBack = {
+                            Log.d("TradeSdk", "Back button pressed. Closing stock details.") // Log when back button is pressed
+                            close()
+                        })
                     }
                 }
             }
+        } else {
+            Log.d("TradeSdk", "Host composable is not shown.") // Log if Host composable isn't shown
         }
     }
 }
